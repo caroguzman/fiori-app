97133b2380addf921847296dd2267b3f2c389cf1
caroguzman <guzmanrodriguez@yahoo.com.ar>
2025-04-09 23:32:50 +0000

    destination configuration

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,6 +5,19 @@ sap.ui.define([
 
     return Controller.extend("trialcaro.invoices.controller.MainView", {
         onInit() {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("/model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");
+        },
+
+        onFilter: function(oEvent){
+
+        },
+        onClearFilter: function(){
+            const oModelSelScreen = this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/CountryKey", "");
         }
     });
 });
